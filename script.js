ad16ee6db9376281827f7aa1d114f73115dff39a
Jon Harlem <jtharlem@gmail.com>
2015-11-13 11:46:46 -0800

    added initial jquery form validation

---
+++ script.js
@@ -0,0 +1,39 @@
+// var form = $('#some-form');
+// var input = $('#some-input');
+
+// input.val(); // Get input value
+// input.getAttr("name"); // Get input name
+// input[0].checked; // Get the checked status of a checkbox or radio button
+// input[0].disabled; // Get input disabled status
+
+function pizzaCost(){
+    var cost = 0;
+    $("input[type='radio']:checked").each(function(){
+        console.log($(this).val())
+        cost+=parseInt($(this).val());
+    });
+    $("#totalCost").text(cost)
+}
+$().ready(function(){
+    $("input[type='radio']").change(function(){
+        pizzaCost()
+    });
+});
+
+//Size
+
+//Crust and sauce
+
+//Toppings
+
+//Extras
+
+//subtotal
+
+//tax of 8%
+
+//Delivery Fee
+
+//total
+
+//suggested tip of 15%
